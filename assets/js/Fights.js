5729a5c0d7cd93072b550e32e0f1c13c4d64dec7
zPierreB <pierre.briere1998@gmail.com>
2022-12-12 19:23:51 +0100

    push fights system

--- assets/js/Fights.js
+++ assets/js/Fights.js
@@ -2,32 +2,59 @@ class Fights {
 
     battlePokemon = () => {
 
-        const search1stPoke = document.getElementById("1stPokemon");
-        const search2ndPoke = document.getElementById("2ndPokemon");
+        const fightButton = document.getElementById("btnFight");
+        
+        fightButton.addEventListener('click', async(event) => {
+            event.preventDefault();
 
-        // search1stPoke.addEventListener('change', (event) => {
-        //     const url = `https://pokeapi.co/api/v2/pokemon/${event.target.value}`;
-        //     const fetchPokemon = async() => {
-        //         const response = await fetch(url)
-        //         return await response.json();
-        //     }
-        //     console.log(fetchPokemon());
-        // })
+            const divForm = document.getElementById("formPokemon");
+            const h3 = document.createElement('h3');
 
-        search1stPoke.addEventListener('change', async(event) => {
-            const url = `https://pokeapi.co/api/v2/pokemon/${event.target.value}`;
-            const response = await fetch(url);
-            return await response.json()
-            .then(async(data) => {
-                let resultsStats;
-                for(const d of data.stats) {
-                    resultsStats += d.base_stat;
-                }
-                console.log(resultsStats);
-            })
+            const input1stPokemon = document.getElementById("1stPokemon").value;
+            const get1stPokeStats = await this.fetch1stPokemon(input1stPokemon);
+
+            const input2ndPokemon = document.getElementById("2ndPokemon").value;
+            const get2ndPokeStats = await this.fetch2ndPokemon(input2ndPokemon);
+
+            if(get1stPokeStats > get2ndPokeStats) {
+                h3.innerHTML = input1stPokemon;
+                divForm.append(h3);
+                console.log(`${input1stPokemon}`)
+            } else if(get1stPokeStats < get2ndPokeStats) {
+                h3.innerHTML = input2ndPokemon;
+                divForm.append(h3);
+                console.log(`${input2ndPokemon}`)
+            }
         })
     }
 
+    fetch1stPokemon = async(url) => {
+        const url1 = `https://pokeapi.co/api/v2/pokemon/${url}`;
+        const response = await fetch(url1);
+        return await response.json()
+        .then(async(data) => {
+            let resultStats = 0;
+            for(const d of data.stats) {
+                resultStats += d.base_stat;
+            }
+            console.log(resultStats)
+            return resultStats;
+            })
+    }
+
+    fetch2ndPokemon = async(url) => {
+        const url2 = `https://pokeapi.co/api/v2/pokemon/${url}`;
+        const response = await fetch(url2);
+        return await response.json()
+        .then(async(data) => {
+            let resultStats2 = 0;
+            for(const d of data.stats) {
+                resultStats2 += d.base_stat;
+            }
+            console.log(resultStats2);
+            return resultStats2;
+            })
+    }
 }
 
 export default Fights;
